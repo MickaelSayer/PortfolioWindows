16aaa878851be8d941243ca3d98c8564caefdab8
freaks <mickael.sayer.dev@gmail.com>
2023-07-30 13:59:11 +0200

    Initialisation du projet Symfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function homeAction(): Response
+    {
+        return $this->render('/portfolio/home.html.twig');
+    }
+}
