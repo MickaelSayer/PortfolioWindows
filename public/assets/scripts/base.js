e2f50218c4824fa8742b042592ada630e230a573
freaks <mickael.sayer.dev@gmail.com>
2023-08-02 13:40:03 +0200

    amélioration du code + fichier

--- public/assets/scripts/base.js
+++ public/assets/scripts/base.js
@@ -8,15 +8,38 @@ function actualiserHorloge() {
 
     var heureActuelle = heure + ":" + minutes;
 
-    // Mettre à jour le contenu de l'élément avec l'heure actuelle
     $("header .horloge .heure").text(heureActuelle);
 }
 
-
 $(document).ready(function() {
-  // Mettre à jour l'heure toutes les secondes (1000 millisecondes)
-  setInterval(actualiserHorloge, 1000);
+    setInterval(actualiserHorloge, 1000);
+    actualiserHorloge();
 
-  // Appeler la fonction une première fois pour éviter un délai initial
-  actualiserHorloge();
+    $('.logo-windows').on('click', function() {
+        $('.nav-link').toggleClass('open-nav');
+        $(this).toggleClass('active');
+    });
+
+    $('.open-session').on('click', function() {
+        $('.nav-link').removeClass('open-nav');
+        $('.logo-windows').removeClass('active');
+    });
+
+    $('nav .sidebar-left ul li').on('click', function() {
+        const data_link = $(this).data('link');
+
+        $('nav .sidebar-left ul li, header .menu-open .menu-active, .logo-windows').removeClass('active');
+        $('#' + data_link).addClass('active');
+        $('.nav-link').removeClass('open-nav');
+        $(this).addClass('active');
+    });
+
+    const nav_link = $('.nav-link');
+    const logo_windows = $('.logo-windows');
+    $(document).on('click', function(event) {
+        if (nav_link.hasClass('open-nav') && !nav_link.is(event.target) && !logo_windows.is(event.target) && nav_link.has(event.target).length === 0) {
+            nav_link.removeClass('open-nav');
+            $('.logo-windows').removeClass('active');
+        }
+    });
 });
