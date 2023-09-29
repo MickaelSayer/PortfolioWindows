8941c0f7f741da430847da840cd22810c83486e9
freaks <mickael.sayer.dev@gmail.com>
2023-09-29 13:06:58 +0200

    test codacy

--- src/Controller/Twig/CustomTwigExtension.php
+++ src/Controller/Twig/CustomTwigExtension.php
@@ -23,7 +23,7 @@ class CustomTwigExtension extends AbstractExtension
      * Renvoie le chemin du fichier JavaScript en fonction du nom du fichier et de l environnement d exécution.
      *
      * @param string $file_name Le nom du fichier JavaScript
-     * @param string $env La variable d environnement
+     * @param string $env       La variable d environnement
      * @return string Le chemin du fichier JavaScript
      */
     public function getPathFileJs(string $file_name, string $env)
